c4ac9edd6b5c8e183eb7d051a01d99c360cf3b92
yhebbat <yhebbat@student.1337.ma>
2022-05-19 16:52:16 +0100

    @@@@

--- parsing/parse_confile/main.cpp
+++ parsing/parse_confile/main.cpp
@@ -12,51 +12,14 @@
 
 #include "parse_confile.hpp"
 
-int	parsing_conf(int ac, char const **av,parse_config *conf)
-{
-	// parse_server_config *conf = new parse_server_config;
-	// parse_config conf;
-
-	if (ac != 2)
-	{
-		std::cout << "Usage: ./parse_confile <path_to_config_file>" << std::endl;
-		return (0);
-	}
-	std::ifstream file(av[1]);
-	if (!file.is_open())
-	{
-		std::cout << "Error: file " << av[1] << " not found" << std::endl;
-		return (0);
-	}
-	std::string line;
-	std::vector<std::string> lines;
-	while (std::getline(file, line))
-		lines.push_back(line);
-	file.close();
-	conf->set_lines(lines);
-	// conf.read_lines();
-	try
-	{
-		conf->start_parsing();
-	}
-	catch (std::runtime_error &e)
-	{
-		std::cout << e.what() << std::endl;
-		return (0);
-	}
-	// conf->read_server();
-	return (1);
-}
-
 int main(int ac, char const **av)
 {
 	parse_config conf;
-	// std::vector<server> test;
+	std::vector<server> test;
 
-	if(!parsing_conf(ac, av, &conf))
+	if(!conf.parsing_conf(ac, av))
 		return (0);
 	conf.read_server();
-	// test = conf.get_server_vect();
-	/****/
+	test = conf.get_server_vect();
 	return (0);
 }
--- parsing/parse_confile/parse_confile.cpp
+++ parsing/parse_confile/parse_confile.cpp
@@ -12,18 +12,6 @@
 
 #include "parse_confile.hpp"
 
-//--------constructor--------
-
-// parse_config::parse_config(): _lines()
-// {
-// }
-
-// //--------destructor--------
-
-// parse_config::~parse_config()
-// {
-// }
-
 //--------methods--------
 
 size_t parse_config::get_lines_size() const
@@ -51,13 +39,6 @@ void    parse_config::accolade_error()
             --accolade;
         ++i;
     }
-    // i = 0;
-    // while (i < _words.size())
-    // {
-    //     std::cout << _words[i] << std::endl;
-    //     ++i;
-    // }
-    // std::cout << accolade << std::endl;
     if (accolade != 0)
         throw std::runtime_error("Error: File Not Well Formated(accolade error)");
 }
@@ -81,7 +62,6 @@ void       parse_config::split_by_space()
 
 unsigned int   parse_config::server_parsing(unsigned int &i)
 {
-    // server *s = new server();
     server s;
     bool location_flag = false;
     bool cgi_flag = false;
@@ -115,6 +95,7 @@ unsigned int   parse_config::server_parsing(unsigned int &i)
            i = s.fill_cgi(_words, i, cgi_flag);
         i++;
     }
+    s.set_to_default();
     this->_servers.push_back(s);
     return i;
 }
@@ -205,10 +186,8 @@ void    parse_config::start_parsing()
 
 void    parse_config::read_server()
 {
-    // std::vector <server> test;
     size_t i = 0;
 
-    // test = _servers;
     while (i < _servers.size())
     {
         std::cout << "--------------SERVER" << i << "---------------" <<std::endl;
@@ -342,6 +321,42 @@ void    parse_config::read_server()
         i++;
     }
 }
+
+int parse_config::basic_error(std::string error_message, char const *av, std::string error_message2)
+{
+    if (av && error_message2 != "" && error_message != "")
+        std::cout << error_message << av << error_message2 << std::endl;
+    else if (av && error_message != "")
+        std::cout << error_message << av << std::endl;
+    else if (error_message != "")
+        std::cout << error_message << std::endl;    
+    return (0);
+}
+
+int parse_config::parsing_conf(int ac, char const **av)
+{
+	if (ac != 2)
+        return (basic_error("Usage: ./parse_confile <path_to_config_file>", NULL, ""));
+	std::ifstream file(av[1]);
+	if (!file.is_open())
+        return (basic_error("Error: file " , av[1] , " not found"));
+	std::string line;
+	std::vector<std::string> lines;
+	while (std::getline(file, line))
+		lines.push_back(line);
+	file.close();
+	set_lines(lines);
+	try
+	{
+		start_parsing();
+	}
+	catch (std::runtime_error &e)
+	{
+		std::cout << e.what() << std::endl;
+		return (0);
+	}
+	return (1);
+}
 //--------getters-------------
 
 std::string parse_config::get_lines(int i) const
@@ -356,145 +371,3 @@ void   parse_config::set_lines(std::vector<std::string> lines)
 }
 
 //--------operators--------
-// std::vector<server>& parse_config::operator=(std::vector<server> const & rhs)
-// {
-//     // std::cout << "assignment operator called"<< std::endl;
-//     if (this->_servers != &rhs)
-//     {
-//         size_t i = 0;
-//     while (i < rhs.size())
-//     {
-//         std::cout << "--------------SERVER" << i << "---------------" <<std::endl;
-//         unsigned int j = 0;
-//         std::cout << "server_names: ";
-//         while (j < this->_servers[i].get_name_size())
-//         {
-//             this->_servers[i].set_name(rhs[i].get_name(j));
-//             j++;
-//         }
-//         std::cout << std::endl;
-//         std::cout << "listen_host: " << this->_servers[i].get_listen_host() << std::endl;
-//         std::cout << "listen_port: " <<this->_servers[i].get_listen_port() << std::endl;
-//         std::cout << "root: " << this->_servers[i].get_root() << std::endl;
-//         j = 0;
-//         std::cout << "allowed_methods: ";
-//         while (j < this->_servers[i].get_allowed_methods_size())
-//         {
-//             std::cout << this->_servers[i].get_allowed_methods(j) << " ";
-//             j++;
-//         }
-//         std::cout << std::endl;
-//         std::cout << "upload_path: " << this->_servers[i].get_upload_path() << std::endl;
-//         j = 0;
-//         std::cout << "index: ";
-//         while (j < this->_servers[i].get_index_size())
-//         {
-//             std::cout << this->_servers[i].get_index(j) << " ";
-//             j++;
-//         }
-//         std::cout << std::endl;
-//         if (this->_servers[i].get_autoindex())
-//             std::cout << "auto index: on" << std::endl;
-//         else
-//             std::cout << "auto index: off" << std::endl;
-//         j = 0;
-//         while (j < _servers[i].get_error_pages_size())
-//         {
-//             std::cout << "error_pages: ";
-//             std::vector<std::string> tmp;
-//             tmp = _servers[i].get_error_pages(j);
-//             std::cout << tmp[0];
-//             std::cout << "   ";
-//             std::cout << tmp[1] << std::endl;
-//             j++;
-//         }
-//         j = 0;
-//         while (j < _servers[i].get_redirections_size())
-//         {
-//             std::cout << "redirections: ";
-//             std::vector<std::string> tmp;
-//             tmp = _servers[i].get_redirections(j);
-//             std::cout << tmp[0];
-//             std::cout << "   ";
-//             std::cout << tmp[1] << std::endl;
-//             j++;
-//         }
-//         std::cout << "client_max_body_size: " << this->_servers[i].get_client_max_body_size() << std::endl;
-//         j = 0;
-//         std::cout << "********location*********" << std::endl;
-//         while (j < _servers[i].get_location_size())
-//         {
-//             location t = this->_servers[i].get_location(j);
-//             std::cout << "location_path: "<< t.get_locations_path() << std::endl;
-//             unsigned int k = 0;
-//             std::cout << "root: "<< t.get_root() << std::endl;
-//             if (t.get_autoindex())
-//                 std::cout << "auto index: on" << std::endl;
-//             else
-//                 std::cout << "auto index: off" << std::endl;
-//             std::cout << "index: ";
-//             while (k < t.get_index_size())
-//             {
-//                 std::cout << t.get_index(k) << " ";
-//                 k++;
-//             }
-//             std::cout << std::endl;
-//             k = 0;
-//             std::cout << "allowed_methods: ";
-//             while (k < t.get_methods_size())
-//             {
-//                 std::cout << t.get_methods(k) << " ";
-//                 k++;
-//             }
-//             if (t.get_cgi_size() != 0)
-//             {
-//                 size_t m = 0;
-//                 std::cout << std::endl;
-//                 std::cout << "********CGI_LOCATION*********" << std::endl;
-//                 while (m < t.get_cgi_size())
-//                 {
-//                     cgi p = t.get_cgi(m);
-//                     std::cout << "cgi name:" << p.get_cgi_name() << std::endl;
-//                     std::cout << "cgi_path: "<< p.get_cgi_path() << std::endl;
-//                     unsigned int k = 0;
-//                     k = 0;
-//                     std::cout << "cgi_methods: ";
-//                     while (k < p.get_cgi_methods_size())
-//                     {
-//                         std::cout << p.get_cgi_methods(k) << " ";
-//                         k++;
-//                     }
-//                     std::cout << std::endl;
-//                     std::cout << "***********************" << std::endl;
-//                     m++;
-//                 }
-//             }
-//             std::cout << std::endl;
-//             std::cout << "***********************" << std::endl;
-//             j++;
-//         }
-//         j = 0;
-//         std::cout << "********CGI*********" << std::endl;
-//         while (j < _servers[i].get_cgi_size())
-//         {
-//             cgi t = this->_servers[i].get_cgi(j);
-//             std::cout << "cgi name:" << t.get_cgi_name() << std::endl;
-//             std::cout << "cgi_path: "<< t.get_cgi_path() << std::endl;
-//             unsigned int k = 0;
-//             k = 0;
-//             std::cout << "cgi_methods: ";
-//             while (k < t.get_cgi_methods_size())
-//             {
-//                 std::cout << t.get_cgi_methods(k) << " ";
-//                 k++;
-//             }
-//             std::cout << std::endl;
-//             std::cout << "***********************" << std::endl;
-//             j++;
-//         }
-//         i++;
-//     }
-//     }
-//         // this->_value = rhs._value;
-//     return (*this);
-// }
