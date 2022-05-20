4ff89e26a8b5ad43aac510c2c7f65f5948e5cb87
ELORCHI <elorchie@gmail.com>
2022-05-20 13:19:39 +0100

    lets go

---
+++ response/includes/response.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+class response
+{
+    public:
+            //response constructor takes a request and servers vector
+            //identify which server to handle the request
+            //generate a response object <still ambiguous> needs some steps to do
+    private:
+
+};
