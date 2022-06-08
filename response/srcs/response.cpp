2a44a70c43ef9bfff645211d7e8fcc3d11660ff4
ELORCHI <elorchie@gmail.com>
2022-06-08 08:39:31 +0100

    shiiit

--- response/srcs/response.cpp
+++ response/srcs/response.cpp
@@ -20,10 +20,80 @@ std::string    formatted_time(void)
 
 void responseHandler::setResposeStatusLine(int status, std::string status_line)
 {
-	char *tmp;
 
 	buffer = HTTP_VERSION_1_1 + std::string(" ") + status_line + std::string("\n");
 	bufferOffset = buffer.size();
+}
+
+std::string responseHandler::getexten(void)
+{
+	std::string res;
+
+	// set res to get file ext
+    if (!res.empty())
+	{
+		if (res == ".html") return "text/html";
+		else if (res == ".htm") return "text/html";
+		else if (res == ".shtml") return "text/html";
+		else if (res == ".css") return "text/css";
+		else if (res == ".xml") return "text/xml";
+		else if (res == ".csv") return "text/csv";
+		else if (res == ".gif") return "image/gif";
+		else if (res == ".ico") return "image/x-icon";
+		else if (res == ".jpeg") return "image/jpeg";
+		else if (res == ".jpg") return "image/jpeg";
+		else if (res == ".js") return "application/javascript";
+		else if (res == ".json") return "application/json";
+		else if (res == ".png") return "image/png";
+		else if (res == ".pdf") return "application/pdf";
+		else if (res == ".svg") return "image/svg+xml";
+		else if (res == ".txt") return "text/plain";
+		else if (res == ".atom") return "application/atom+xml";
+		else if (res == ".rss") return "application/rss+xml";
+		else if (res == ".webp") return "image/webp";
+		else if (res == ".3gpp") return	"video/3gpp";
+		else if (res == ".3gp") return "video/3gpp";
+		else if (res == ".ts") return "video/mp2t";
+		else if (res == ".mp4") return "video/mp4";
+		else if (res == ".mpeg") return "video/mpeg";
+		else if (res == ".mpg") return "video/mpeg";
+		else if (res == ".mov") return "video/quicktime";
+		else if (res == ".webm") return "video/webm";
+		else if (res == ".flv") return "video/x-flv";
+		else if (res == ".m4v") return "video/x-m4v";
+		else if (res == ".mng") return "video/x-mng";
+		else if (res == ".asx") return "video/x-ms-asf";
+		else if (res == ".asf") return "video/x-ms-asf";
+		else if (res == ".wmv") return "video/x-ms-wmv";
+		else if (res == ".avi") return "video/x-msvideo";
+		else if (res == ".mml") return "text/mathml";
+		else if (res == ".txt") return "text/plain";
+		else if (res == ".jad") return "text/vnd.sun.j2me.app-descriptor";
+		else if (res == ".wml") return "text/vnd.wap.wml";
+		else if (res == ".htc") return "text/x-component";
+		else if (res == ".png") return "image/png";
+		else if (res == ".tif") return "image/tiff";
+		else if (res == ".tiff") return "image/tiff";
+		else if (res == ".wbmp") return "image/vnd.wap.wbmp";
+		else if (res == ".ico") return "image/x-icon";
+		else if (res == ".jng") return "image/x-jng";
+		else if (res == ".bmp") return "image/x-ms-bmp";
+		else if (res == ".svg") return "image/svg+xml";
+		else if (res == ".svgz") return "image/svg+xml";
+		else if (res == ".mid") return "audio/midi";
+		else if (res == ".midi") return "audio/midi";
+		else if (res == ".kar") return "audio/midi";
+		else if (res == ".mp3") return "audio/mpeg";
+		else if (res == ".ogg") return "audio/ogg";
+		else if (res == ".m4a") return "audio/x-m4a";
+		else if (res == ".ra") return "audio/x-realaudio";
+		else if (res == ".pl") return "application/x-perl";
+		else if (res == ".py") return "application/x-python";
+		else if (res == ".php") return "application/x-php";
+		else if (res == ".cpp") return "application/x-c++";
+		else if (res == ".c") return "application/x-c";
+	}
+    return "plain/text";
 }
 
 void responseHandler::setResponseHeaders(void)
@@ -41,8 +111,7 @@ void responseHandler::setResponseHeaders(void)
 
 void responseHandler::setResponseBody(std::string body)
 {
-	// if default error exits read from defaul
-	//else buffer += body
+
 }
 
 void responseHandler::setClient(client &_cl)
