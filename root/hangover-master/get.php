a00c520e5519d5d668e1c41fc1a9a25e9b20acdb
ELORCHI <elorchie@gmail.com>
2022-06-20 01:00:11 +0100

    deleted

--- root/hangover-master/get.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-
-Welcome from PHP: <?php echo $_GET['name']; ?><br>
-Your email address is: <?php echo $_GET['email']; ?>
-<?php print_r($_FILES) ?>
-</body>
-</html> 
--- root/hangover-master/ls/get.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-
-Welcome from PHP: <?php echo $_GET['name']; ?><br>
-Your email address is: <?php echo $_GET['email']; ?>
-
-</body>
-</html> 
